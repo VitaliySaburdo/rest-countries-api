af57bac5125a26990a969d8211971b48f2de81f3
VitaliySaburdo <vsaburdo@gmail.com>
2024-09-04 18:43:52 +0300

    add header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,19 @@
 import { Container } from '../Container';
+import Moon from '../../assets/images/moon.png';
+import style from './Header.module.scss';
 
 export const Header = () => {
   return (
     <>
       <Container>
         <header>
-          <h1>Were in the world?</h1>
+          <h1 className={style.title}>Were in the world?</h1>
+          <div className={style.wrapper}>
+            <button className={style.btn}>
+              <img src={Moon} alt="moon" />
+            </button>
+            <p>Dark mode</p>
+          </div>
         </header>
       </Container>
     </>
