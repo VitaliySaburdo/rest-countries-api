d65906d47b1290684a9506d9122b4cc33ffc404c
VitaliySaburdo <vsaburdo@gmail.com>
2024-09-06 06:45:50 +0300

    info

--- src/components/CountryItem/CountryItem.tsx
+++ src/components/CountryItem/CountryItem.tsx
@@ -9,6 +9,9 @@ export const CountryItem: React.FC<CartItemProps> = ({ country }) => {
     <li>
       <img src={country.flags.png} alt="flag image" />
       <h2>{country.name.common}</h2>
+      <p>Population: {country.population}</p>
+      <p>Region: {country.region}</p>
+      <p>Capital: {country.capital}</p>
     </li>
   );
 };
