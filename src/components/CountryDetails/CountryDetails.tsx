b78f6f8215a9b9ecff18e51f39f5c48db7e9d32b
Vitaliy Saburdo <vsaburdo@gmail.com>
2024-09-12 16:10:21 +0300

    css detail

--- src/components/CountryDetails/CountryDetails.tsx
+++ src/components/CountryDetails/CountryDetails.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { Country } from '../../../types/Country';
+import style from './CountryDetails.module.scss';
 
 interface CountryDetailsProps {
   country: Country;
@@ -39,31 +40,49 @@ export const CountryDetails: React.FC<CountryDetailsProps> = ({
 
   return (
     <>
-      <img src={png} alt="flag" />
-      <h2>{common}</h2>
-      <p>Native Name: {nativeName[keyNativeName].common}</p>
-      <p>Population: {formattedNumber}</p>
-      <p>Region: {region}</p>
-      <p>Sub Region: {subregion}</p>
-      <p>Capital: {capital}</p>
-      <p>Top Level Domain: {altSpellings[0].toLowerCase()}</p>
-      <p>Currencies: {currencies[keyCurrencies].name}</p>
-      <p>Languages: {lang}</p>
-
-      <ul>
-        Border Countries:
-        {borderCountries.length
-          ? borderCountries.map((country) => {
-              return (
-                <Link key={country} to={`/country/${country}`}>
-                  <li>
-                    <p>{country}</p>
-                  </li>
-                </Link>
-              );
-            })
-          : '  none'}
-      </ul>
+      <div className={style.wrapper}>
+        <img className={style.img} src={png} alt="flag" />
+        <div style={{ flex: '1' }}>
+          <h2 className={style.title}>{common}</h2>
+          <div className={style.textWrapper}>
+            <div>
+              {' '}
+              <p>
+                <span className={style.boldTxt}>Native Name: </span>
+                {nativeName[keyNativeName].common}
+              </p>
+              <p>
+                {' '}
+                <span className={style.boldTxt}>Population: </span>{' '}
+                {formattedNumber}
+              </p>
+              <p>Region: {region}</p>
+              <p>Sub Region: {subregion}</p>
+              <p>Capital: {capital}</p>
+            </div>
+            <div>
+              {' '}
+              <p>Top Level Domain: {altSpellings[0].toLowerCase()}</p>
+              <p>Currencies: {currencies[keyCurrencies].name}</p>
+              <p>Languages: {lang}</p>
+            </div>
+          </div>
+          <ul>
+            Border Countries:
+            {borderCountries.length
+              ? borderCountries.map((country) => {
+                  return (
+                    <Link key={country} to={`/country/${country}`}>
+                      <li>
+                        <p>{country}</p>
+                      </li>
+                    </Link>
+                  );
+                })
+              : '  none'}
+          </ul>
+        </div>
+      </div>
     </>
   );
 };
